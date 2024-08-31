d19b792d2c3e2157a928ab130487a19337d7fd3d
ChandGoyal <chandgoyal017@gmail.com>
2024-08-31 22:36:31 +0530

    Added Redux Store and Routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddUser from "./pages/AddUser";
+import EditUser from "./pages/EditUser";
+import Notification from "./components/Notification";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto p-4">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Home />
+              <Notification />
+            </>
+          }
+        />
+        <Route
+          path="/add"
+          element={
+            <>
+              <AddUser />
+              <Notification />
+            </>
+          }
+        />
+        <Route
+          path="/edit/:id"
+          element={
+            <>
+              <EditUser />
+              <Notification />
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 }
